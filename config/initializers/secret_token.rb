403538fda76a2a3b686bdcd501c511c8f1699d45
Luci Temple <hello@lucitemple.com.au>
2013-11-16 14:31:10 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '7cd5896db86e2eced0ce709b38e2ab9290150498623113513b02d1241b147e02fbeca70d3597cd72b04b9e292bc7bed838c02520cbfa266b9d248f2b1e875dc6'
